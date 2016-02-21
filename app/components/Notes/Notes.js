b18dc3bd42f68aca27c9afdc28898dc70e8d696a
macarenapoo <macarenapoo@gmail.com>
2016-02-21 15:29:23 -0800

    Add notes through input field

--- app/components/Notes/Notes.js
+++ app/components/Notes/Notes.js
@@ -1,15 +1,18 @@
 var React = require('react'),
-    NotesList = require('./NotesList');
+    NotesList = require('./NotesList'),
+    AddNote = require('./AddNote');
 
 var Notes = React.createClass({
   propTypes: {
     username: React.PropTypes.string.isRequired,
-    notes: React.PropTypes.array.isRequired
+    notes: React.PropTypes.array.isRequired,
+    addNote: React.PropTypes.func.isRequired
   },
   render: function(){
     return (
       <div>
         <h3> Notes for {this.props.username} </h3>
+        <AddNote username={this.props.username} addNote={this.props.addNote} />
         <NotesList notes={this.props.notes} />
       </div>
     )
