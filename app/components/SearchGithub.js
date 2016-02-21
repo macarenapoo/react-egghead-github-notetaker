3abfc1a926db28bc74293d87b7d305001b5a46e3
macarenapoo <macarenapoo@gmail.com>
2016-02-21 15:56:49 -0800

    Github Search, route to profile

---
+++ app/components/SearchGithub.js
@@ -0,0 +1,30 @@
+var React = require('react'),
+    Router = require('react-router');
+
+var SearchGithub = React.createClass({
+  mixins: [Router.History],
+  getRef: function(ref){
+    this.usernameref = ref;
+  },
+  handleSubmit: function(){
+    var username = this.usernameref.value;
+    this.usernameref.value = "";
+    this.history.pushState(null, "profile/" + username);
+  },
+  render: function(){
+    return(
+      <div className="col-sm-12">
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-group col-sm-7">
+            <input type="text" className="form-control" ref={this.getRef} />
+          </div>
+          <div className="form-group col-sm-5">
+            <button type="submit" className="btn btn-block btn-primary">Search Github</button>
+          </div>
+        </form>
+      </div>
+    )
+  }
+});
+
+module.exports = SearchGithub;
